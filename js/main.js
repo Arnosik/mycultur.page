64fba13efc5f685401cb1c67d4092a097a47c516
Arnosik <gsstslmni@gmail.com>
2025-01-14 16:50:58 +0700

    home section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-function summa(arr) {
-
-    var sum = 0;
-    
-    for(var i = 0; i < arr.length; i++)
-        sum += arr[i];
-
-    return sum;
-    
-}
-    
-var array = [60, 8, 3];
-var res = summa(array);
-console.log(res);
 
 
 
